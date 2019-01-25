9e5b510f7b44f4f66998a6fdd5f6e61ee668da82
Nakeem Purkiss <npurkiss@oc5165760005.ibm.com>
2019-01-25 11:13:36 -0500

    Add generated delivery build cookbook

---
+++ .delivery/build_cookbook/metadata.rb
@@ -0,0 +1,8 @@
+name 'build_cookbook'
+maintainer 'The Weather Company, an IBM Business'
+maintainer_email 'Nakeem.Purkiss@ibm.com'
+license 'all_rights'
+version '0.1.0'
+chef_version '>= 13.0'
+
+depends 'delivery-truck'
